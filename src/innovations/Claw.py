a7d01bb2d681bf9abef2bc0caffe01ef22ce0d86
Subesh Sukumaran <subesh.sukumaran@student.education.wa.edu.au>
2025-12-12 14:00:46 +0800

    created classes for the servo and force sensor. Yet to make the standard pressure sensor-fingertip pressure sensor interactions. Subtract finger from average of the standard and create a percentage from the results. andre

--- src/innovations/Claw.py
+++ src/innovations/Claw.py
@@ -3,22 +3,24 @@ import time
 
 class Force:
     def __init__(self) -> None:
-        self.pin = 27
+        self.fing_pin = 26
+        self.const_pin = 27
         self.max = 65535
         self.min = 13000
-        self.force = machine.ADC(machine.Pin(self.pin))
-    def read(self) -> int:
-        force_value = self.force.read_u16()
+        self.force = machine.ADC(machine.Pin(self.fing_pin))
+        self.standard = machine.ADC(machine.Pin(self.const_pin))
+    def read(self, sensor) -> int:
+        force_value = sensor.read_u16()
         return force_value
     def get_percentage(self) -> float:
-        force_value = self.read()
+        force_value = self.read(self.force) - self.read(self.standard)
         percentage = (force_value - self.min) / (self.max - self.min) * 100
         percentage = max(0, min(100, percentage))
         return percentage
 
 class Servo:
     def __init__(self) -> None:
-        self.pin = 19
+        self.pin = 0
         self.deg0 = 1638
         self.deg180 = 8191
         self.servo = machine.PWM(machine.Pin(self.pin))
@@ -34,6 +36,7 @@ force = Force()
 vals = []
 state = "close"
 angle = 0.0
+diff = 1
 start_time = time.ticks_ms()
 while True:
     if state == "close":
@@ -42,9 +45,11 @@ while True:
         if f >= 70:
             state = "hold"
             print("Closing force reached:", f)
-        angle += 1
-        if angle > 180:
+        angle += diff
+        if angle >= 180:
             end_time = time.ticks_ms()
-            break
-duration = end_time - start_time
-print("Claw closed. Duration (ms):", duration)
+            diff -= 2
+        if angle <= 0:
+            end_time = time.ticks_ms()
+            diff += 2
+        time.sleep_ms(50)
